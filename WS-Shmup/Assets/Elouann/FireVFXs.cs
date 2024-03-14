f2c23dc898ad2143e65486882234547ab7a07ee9
Elouann <elouann.jouenne@e-artsup.net>
2024-03-14 11:26:17 +0100

    Finished VFXs ship script + stuff

--- WS-Shmup/Assets/Elouann/FireVFXs.cs
+++ WS-Shmup/Assets/Elouann/FireVFXs.cs
@@ -23,7 +23,8 @@ public class FireVFXs : MonoBehaviour
         new Vector3(0.333f, 1, 0), // top
         new Vector3(1, 1, 0), // top right
         new Vector3(1, 0.333f, 0), // right
-        new Vector3(1, -1, 0)  // bottom right
+        new Vector3(1, -1, 0),  // bottom right
+        new Vector3(-0.333f, -0.333f) // standing
     };
 
     private List<Vector3> _newRandomVelocitiesMax = new List<Vector3>()
@@ -35,7 +36,8 @@ public class FireVFXs : MonoBehaviour
         new Vector3(-0.333f, 2, 0), // top
         new Vector3(2, 2, 0), // top right
         new Vector3(2, -0.333f, 0), // right
-        new Vector3(2, -2, 0)  // bottom right
+        new Vector3(2, -2, 0),  // bottom right
+        new Vector3(0.333f, -0.5f) // standing
     };
 
     private void Awake()
@@ -49,44 +51,49 @@ public class FireVFXs : MonoBehaviour
     { 
         switch (direction)
         {
-            case Vector2 _direction when (_direction.y >= -1 && _direction.y < 0): // bottom
-                /*_effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[0]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[0])0;*/
+            case Vector2 _direction when (_direction.y >= -1 && _direction.y < 0) && (_direction.x == 0): // bottom
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[4]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[4]);
                 break;
 
             case Vector2 _direction when (_direction.x >= -1 && _direction.x < 0) && (_direction.y >= -1 && _direction.y < 0): // bottom left
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[1]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[1]);
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[5]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[5]);
                 break;
 
-            case Vector2 _direction when (_direction.x >= -1 && _direction.x < 0): // left
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[2]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[2]);
+            case Vector2 _direction when (_direction.x >= -1 && _direction.x < 0) && (direction.y == 0): // left
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[6]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[6]);
                 break;
 
             case Vector2 _direction when (_direction.x >= -1 && _direction.x < 0) && (_direction.y <= 1 && _direction.y > 0): // top left
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[3]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[3]);
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[7]);
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[7]);
                 break;
 
-            case Vector2 _direction when (_direction.y <= 1 && _direction.y > 0): // top
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[4]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[4]);
+            case Vector2 _direction when (_direction.y <= 1 && _direction.y > 0) && (_direction.x == 0): // top
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[0]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[0]);
                 break;
 
             case Vector2 _direction when (_direction.x <= 1 && _direction.x > 0) && (_direction.y <= 1 && _direction.y > 0): // top right
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[5]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[5]);
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[1]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[1]);
                 break;
 
-            case Vector2 _direction when (_direction.x <= 1 && _direction.x > 0): // right
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[6]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[6]);
+            case Vector2 _direction when (_direction.x <= 1 && _direction.x > 0) && (direction.y == 0): // right
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[2]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[2]);
                 break;
 
             case Vector2 _direction when (_direction.x <= 1 && _direction.x > 0) && (_direction.y >= -1 && _direction.y < 0): // bottom right
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[7]);
-                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMax[7]);
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[3]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[3]);
+                break;
+
+            default: // standing
+                _effect.SetVector3("RandomVelocityMin", _newRandomVelocitiesMin[8]);
+                _effect.SetVector3("RandomVelocityMax", _newRandomVelocitiesMax[8]);
                 break;
         }
     }
